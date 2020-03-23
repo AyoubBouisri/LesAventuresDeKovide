742ab26f59a55adce465b1e29ab4bfa0d76ae774
AyoubBouisri <ayoubbouisri@hotmail.fr>
2020-03-22 23:11:34 -0400

    Added button text

--- LesAventuresDeKovide/Button.js
+++ LesAventuresDeKovide/Button.js
@@ -1,9 +1,9 @@
- function Button(x, y, w, h, func, item) {
+ function Button(x, y, w, h, func, item, txt) {
      this.x = x;
      this.y = y;
      this.w = w;
      this.h = h;
-
+     this.txt = txt;
      this.img = button_img.get();
      this.img.resize(this.w, this.h);
 
@@ -14,6 +14,8 @@
 
      this.item = item;
      this.show = function() {
+         fill(255);
+
          if (this.is_hovered) {
 
              var x = this.x - 5;
@@ -21,11 +23,25 @@
              image(this.hovered_img, x, y);
              this.w = w + 10;
              this.h = h + 10;
+             textSize(34);
+             if (this.txt != null) {
+                 rect
+                 text(this.txt, x, y, this.w, this.h);
+             }
          } else {
              this.w = w;
              this.h = h;
              image(this.img, this.x, this.y);
+             textSize(30);
+             if (this.txt != null) {
+                 rect
+                 text(this.txt, this.x, this.y, this.w, this.h);
+             }
          }
+
+
+
+
 
      }
      this.click = function() {
